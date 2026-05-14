8e526081da2dbebd0a823e533bc8115cebb02862
PrestonV <PrestonViado@outlook.com>
2026-05-14 16:22:52 -0400

    Created another login test

---
+++ tests/login-empty.spec.ts
@@ -0,0 +1,11 @@
+import {test, expect} from '@playwright/test';
+
+test('Login with empty username/password', async ({page}) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('[data-test="username"]').click();
+    await page.locator('[data-test="username"]').fill('');
+    await page.locator('[data-test="password"]').click();
+    await page.locator('[data-test="password"]').fill('');
+    await page.locator('[data-test="login-button"]').click();
+    await expect(page.locator('[data-test="error"]')).toHaveText('Epic sadface: Username is required');
+});
